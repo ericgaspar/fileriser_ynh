e71d35e01da3cbbb3b6e242efbad45530ce3e4df
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-06-24 16:13:04 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -20,7 +20,7 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
 // Define constants
 define('PERSISTENT_TOKENS_KEY', '__KEY__');
 define('PROJECT_ROOT', '__INSTALL_DIR__');
-define('UPLOAD_DIR',    '__DATA_DIR__/');
+define('UPLOAD_DIR',    '__DATA_DIR__/uploads');
 define('USERS_DIR',     '__DATA_DIR__/users/');
 define('USERS_FILE',    'users.txt');
 define('META_DIR',      '/var/www/metadata/');
